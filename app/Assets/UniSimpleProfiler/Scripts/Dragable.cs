a79a1b03f6a35e1dfe1f4f506f956c47b4037d93
baba_s <shotababa1206@gmail.com>
2019-04-08 14:28:06 +0900

    first commit

---
+++ app/Assets/UniSimpleProfiler/Scripts/Dragable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UniSimpleProfiler.Internal
+{
+	/// <summary>
+	/// UI をドラッグできるようにするコンポーネント
+	/// </summary>
+	[DisallowMultipleComponent]
+	public sealed class Dragable : MonoBehaviour, IDragHandler
+	{
+		//====================================================================================
+		// 変数(SerializeField)
+		//====================================================================================
+		[SerializeField, HideInInspector] private RectTransform m_rectTransform = null;
+
+		//====================================================================================
+		// 関数
+		//====================================================================================
+		/// <summary>
+		/// リセットされる時に呼び出されます
+		/// </summary>
+		private void Reset()
+		{
+			m_rectTransform = GetComponent<RectTransform>();
+		}
+
+		/// <summary>
+		/// ドラッグ中に呼び出されます
+		/// </summary>
+		public void OnDrag( PointerEventData e )
+		{
+			m_rectTransform.position += new Vector3( e.delta.x, e.delta.y, 0f );
+		}
+	}
+}
---
+++ app/Assets/UniSimpleProfiler/Scripts/UniSimpleProfilerUI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using TMPro;
+using UniSimpleProfiler.Internal;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace UniSimpleProfiler
+{
+	/// <summary>
+	/// シンプルなプロファイラの UI を管理するクラス
+	/// </summary>
+	[DisallowMultipleComponent]
+	public sealed partial class UniSimpleProfilerUI : MonoBehaviour
+	{
+		//====================================================================================
+		// 定数
+		//====================================================================================
+		private const int INTERVAL = 0;
+
+		//====================================================================================
+		// 変数(SerializeField)
+		//====================================================================================
+		[SerializeField] private TextMeshProUGUI	m_fpsTextUI				= null;
+		[SerializeField] private TextMeshProUGUI	m_gcTextUI				= null;
+		[SerializeField] private TextMeshProUGUI	m_monoUsedTextUI		= null;
+		[SerializeField] private TextMeshProUGUI	m_monoTotalTextUI		= null;
+		[SerializeField] private TextMeshProUGUI	m_unityUsedTextUI		= null;
+		[SerializeField] private TextMeshProUGUI	m_unityTotalTextUI		= null;
+
+		//====================================================================================
+		// 変数(readonly)
+		//====================================================================================
+		private readonly FPSCounter	m_fPSCounter = new FPSCounter();
+
+		//====================================================================================
+		// 変数
+		//====================================================================================
+		private float m_timer;
+
+		//====================================================================================
+		// 関数
+		//====================================================================================
+		/// <summary>
+		/// 更新される時に呼び出されます
+		/// </summary>
+		private void Update()
+		{
+			m_fPSCounter.Update();
+
+			m_timer += Time.unscaledDeltaTime;
+			if ( m_timer < INTERVAL ) return;
+			m_timer -= INTERVAL;
+
+			var monoUsed	= ( Profiler.GetMonoUsedSizeLong()			>> 10 ) / 1024f;
+			var monoTotal	= ( Profiler.GetMonoHeapSizeLong()			>> 10 ) / 1024f;
+			var unityUsed	= ( Profiler.GetTotalAllocatedMemoryLong()	>> 10 ) / 1024f;
+			var unityTotal	= ( Profiler.GetTotalReservedMemoryLong()	>> 10 ) / 1024f;
+
+			m_fpsTextUI			.SetText( "{0}", ( int ) m_fPSCounter.Fps	);
+			m_gcTextUI			.SetText( "{0}", GC.CollectionCount( 0 )	);
+			m_monoUsedTextUI	.SetText( "{0}", ( int ) monoUsed			);
+			m_monoTotalTextUI	.SetText( "{0}", ( int ) monoTotal			);
+			m_unityUsedTextUI	.SetText( "{0}", ( int ) unityUsed			);
+			m_unityTotalTextUI	.SetText( "{0}", ( int ) unityTotal			);
+		}
+	}
+}
